8629d46f297c96cf27fb67b7003a9d8e79a0c773
cueo <mohit.ritanil@gmail.com>
2022-02-22 20:50:32 -0500

    Playground update

--- hw1/play.py
+++ hw1/play.py
@@ -1,10 +1,24 @@
+from pprint import pprint
+
 import numpy as np
 import pandas as pd
 
 
-def main():
+def check_labels():
     df = pd.read_csv('train.csv')
     print(np.unique(df['emotions'], return_counts=True))
+
+
+def check_results():
+    filename = 'lr_plot_data.pkl'
+    with open(f'models/{filename}', 'rb') as f:
+        data = pd.read_pickle(f)
+        pprint(data)
+
+
+def main():
+    # check_labels()
+    check_results()
 
 
 if __name__ == '__main__':
