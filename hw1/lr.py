d8ce0113b4640fa0a5e9c2c17db1a5d19e98fd17
cueo <mohit.ritanil@gmail.com>
2022-02-22 20:46:42 -0500

    Update Logistic Regression

--- hw1/lr.py
+++ hw1/lr.py
@@ -1,85 +1,49 @@
 import numpy as np
-from scipy.special import softmax
-
-from const import labels_dict
+from sklearn.metrics import f1_score
 
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
-def encode_onehot(a):
-    """
-    Create one-hot encoding of the labels with rows as labels and columns as data rows.
-    Args:
-        a: labels
+def softmax(X):
+    ps = np.empty(X.shape)
+    for i in range(X.shape[0]):
+        ps[i, :] = np.exp(X[i, :])
+        ps[i, :] /= np.sum(ps[i, :])
+    return ps
 
-    Returns:
-        one-hot encoding of the labels
-    """
-    a_onehot = np.zeros((a.size, len(labels_dict)))
-    for x in a.iteritems():
-        a_onehot[x[0], x[1]] = 1
-    return a_onehot
 
 # noinspection PyPep8Naming
 class Logistic:
-    def __init__(self, data, labels):
-        self.X = data.reset_index(drop=True)
-        self.Y = encode_onehot(labels.reset_index(drop=True))
-        self.weights = np.zeros((self.X.shape[1], self.Y.shape[1]))
-        # self.bias = np.zeros(self.Y.shape[0])
+    def __init__(self, data, labels, learning_rate=0.1, max_epochs=200, lam=0.001):
+        self.X = data
+        self.Y = labels
 
-        # self.method = None
-        # self.n_features = 5000
-        # self.vectorizer = None
+        self.learning_rate = learning_rate
+        self.max_epochs = max_epochs
+        self.lam = lam
 
-    # def feature_extraction(self, data, train=False):
-    #     data, vectorizer = util.extract_features(data, self.method, self.n_features, self.vectorizer, train)
-    #     self.vectorizer = vectorizer
-    #     return data
-
-    def logistic_loss(self, predicted_label, true_label):
-        pass
-
-    def regularizer(self, lam=0.03):
-        return self.weights * lam
+        self.weights = np.random.rand(self.X.shape[1], self.Y.shape[1])
 
     def gradient(self, lam):
         z = -self.X.dot(self.weights)
-        # TODO: implement softmax
-        p = softmax(z, axis=1)
-        return (self.X.T.dot(self.Y - p) + lam * self.weights) / self.X.shape[0]
+        p = softmax(z)
+        return (self.X.T.dot(self.Y - p)) / self.X.shape[0]
 
-    def update_weights(self, new_weights):
-        self.weights = new_weights
+    def fit(self):
+        for epoch in range(self.max_epochs):
+            self.weights -= self.learning_rate * self.gradient(self.lam) + self.lam * self.weights
 
-    def update_bias(self, new_bias):
-        self.bias = new_bias
-
-    def predict_labels(self, data_point):
-        y_pred = np.dot(data_point, self.weights) + self.bias
-        return sigmoid(y_pred)
-
-    def fit(self, learning_rate=0.005, max_epochs=500, lam=0.05):
-        # self.method = feature_method
-        # labeled_data = self.feature_extraction(labeled_data, train=True)
-        ctr = 0
-        prev_loss = 0
-        epoch = 0
-        for epoch in range(max_epochs):
-            loss = 0
-            self.weights -= learning_rate * self.gradient(lam)
-            # Stop if loss does not change for a number of epochs
-            # if abs(loss - prev_loss) < 0.00001:
-            #     ctr += 1
-            # prev_loss = loss
-            # if ctr == 5:
-            #     break
-        # print(f'Training completed after epoch={epoch}.')
-
-    def predict(self, X):
-        X_r = X.reset_index(drop=True)
-        z = -np.dot(X_r, self.weights)
-        p = softmax(z, axis=1)
-        return np.argmax(p, axis=1)
+    def predict(self, X, Y):
+        z = -np.dot(X, self.weights)
+        p = softmax(z)
+        predictions = np.argmax(p, axis=1)
+        labels = Y
+        if not len(Y.shape) == 1:
+            labels = np.argmax(Y, axis=1)
+        print(f'{np.unique(predictions)} / {np.unique(labels)}')
+        accuracy = np.mean(predictions == labels)
+        f1 = f1_score(labels, predictions, average='macro')
+        print(f'Accuracy: {accuracy} F1: {f1}')
+        return accuracy, f1
