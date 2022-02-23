5f202e12c345f653e09693139ee925e04002eb52
cueo <mohit.ritanil@gmail.com>
2022-02-22 20:49:14 -0500

    Graph logic

--- hw1/report.py
+++ hw1/report.py
@@ -1,64 +1,63 @@
-import json
 import pickle
 
 import matplotlib.pyplot as plt
 
 
-def plot_graph_logistic(plot_data):
-    epochs = [25, 50, 100, 200, 500]
-    lrs = [0.001, 0.005, 0.01, 0.05, 0.1]
-    lams = [0.001, 0.005, 0.01, 0.05, 0.1]
-    plt.style.use('seaborn-whitegrid')
-    figure, axis = plt.subplots(5, 5)
-    figure.tight_layout()
-    # figure.subplots_adjust(hspace=0.5, wspace=0.5)
-    # figure.size = (20, 20)
+def plot_graph(plot_data, model):
+    alphas = {k[1] for k in plot_data.keys()}
+    x = sorted(alphas)
 
-    title = 'Epoch: {} LR={}'
+    if model == 'lr':
+        title = 'Logistic Regression'
+        epoch, alpha, lam = 0, 0, 0
+        params = (epoch, alpha, lam)
+    else:
+        title = 'Neural Network'
+        epoch, alpha, lam, hn = 0, 0, 0, 0
+        params = (epoch, alpha, lam, hn)
+    test_accuracy = 0
+    for k, v in plot_data.items():
+        if v['test_accuracy'] > test_accuracy:
+            params = k
 
-    # accuracies = [plot_data[(epochs[0], lrs[0], lam)][1] for lam in lams]
-    # plt.plot(lams, accuracies)
+    print(f'Parameters: {params}')
 
-    for i, epoch in enumerate(epochs):
-        for j, lr in enumerate(lrs):
-            axis[i, j].set_title(title.format(epoch, lr))
-            axis[i, j].set_xlabel('Lambda')
-            axis[i, j].set_ylabel('Accuracy')
-            axis[i, j].set_ylim(0.75, 1.1)
+    train_accuracies = []
+    test_accuracies = []
+    train_f1s = []
+    test_f1s = []
+    for alpha in x:
+        if model == 'lr':
+            key = (params[0], alpha, params[2])
+        else:
+            key = (params[0], alpha, params[2], params[3])
+        train_accuracies.append(plot_data[key]['train_accuracy'])
+        test_accuracies.append(plot_data[key]['test_accuracy'])
+        train_f1s.append(plot_data[key]['train_f1'])
+        test_f1s.append(plot_data[key]['test_f1'])
 
-            train_accuracies = [plot_data[(epoch, lr, lam)][0] for lam in lams]
-            val_accuracies = [plot_data[(epoch, lr, lam)][1] for lam in lams]
-            axis[i, j].plot(lams, train_accuracies, label='Training Accuracy')
-            axis[i, j].plot(lams, val_accuracies, label='Validation Accuracy')
-    plt.legend()
-    plt.show()
+    # draw(x, train_accuracies, test_accuracies, title, 'Accuracy')
+    draw(x, train_f1s, test_f1s, title, 'F1 Score')
 
 
-def plot_graph_ensemble(plot_data):
-    num_clfs = list(range(3, 11))
-    plt.style.use('seaborn-whitegrid')
-    # figure.subplots_adjust(hspace=0.5, wspace=0.5)
-    # figure.size = (20, 20)
-
-    plt.xlabel('Number of Classifiers')
-    plt.ylabel('Accuracy')
-    train_accuracies = [plot_data[num_clf][0] for num_clf in num_clfs]
-    val_accuracies = [plot_data[num_clf][1] for num_clf in num_clfs]
-    plt.plot(num_clfs, train_accuracies, label='Training Accuracy')
-    plt.plot(num_clfs, val_accuracies, label='Validation Accuracy')
-    plt.legend()
+def draw(x, y1, y2, title, label):
+    fig, ax = plt.subplots()
+    ax.plot(x, y1, '-o', label=f'Train {label}')
+    ax.plot(x, y2, '-o', label=f'Validation {label}')
+    ax.set_xlabel('Learning Rate')
+    ax.set_ylabel(f'{label}')
+    ax.set_title(title)
+    ax.legend()
     plt.show()
 
 
 def main():
-    model = 'e'
-    filename = 'plot_data.pkl' if model == 'l' else 'ensemble_plot_data.pkl'
-    with open(filename, 'rb') as f:
+    # model = 'lr'
+    model = 'nn'
+    filename = 'lr_plot_data_2.pkl' if model == 'lr' else 'nn_plot_data_2.pkl'
+    with open(f'models/{filename}', 'rb') as f:
         plot_data = pickle.load(f)
-    if model == 'l':
-        plot_graph_logistic(plot_data)
-    else:
-        plot_graph_ensemble(plot_data)
+    plot_graph(plot_data, model)
 
 
 if __name__ == '__main__':
